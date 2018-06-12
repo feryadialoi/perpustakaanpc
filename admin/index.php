ceb60d9e3a09b4c333c5cdd824c8c49cef26c667
Feryadi <feryadialoi@gmail.com>
2018-06-13 06:49:33 +0700

    update realtime Time php js: admin, user

--- admin/index.php
+++ admin/index.php
@@ -29,7 +29,7 @@
   <link href="../assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" >
   <link rel="shortcut icon" href="../assets/img/logo.png">
 </head>
-<body>
+<body onload="setInterval('displayServerTime()', 1000);">
   <!-- topbar menu start -->
   <div class="top-bar">
     <div class="hamburger">
@@ -39,6 +39,31 @@
       <button class="hamburger-button" onclick="document.getElementById('modallogout').style.display='block'" ><i class="material-icons logout-button">exit_to_app</i> Logout</button>
     </div>
     <div class="top-bar-menu">
+      <script type="text/javascript">
+          //set timezone
+          <?php date_default_timezone_set('Asia/Jakarta'); ?>
+          //buat object date berdasarkan waktu di server
+          var serverTime = new Date(<?php print date('Y, m, d, H, i, s, 0'); ?>);
+          //buat object date berdasarkan waktu di client
+          var clientTime = new Date();
+          //hitung selisih
+          var Diff = serverTime.getTime() - clientTime.getTime();
+          //fungsi displayTime yang dipanggil di bodyOnLoad dieksekusi tiap 1000ms = 1detik
+          function displayServerTime(){
+              //buat object date berdasarkan waktu di client
+              var clientTime = new Date();
+              //buat object date dengan menghitung selisih waktu client dan server
+              var time = new Date(clientTime.getTime() + Diff);
+              //ambil nilai jam
+              var sh = time.getHours().toString();
+              //ambil nilai menit
+              var sm = time.getMinutes().toString();
+              //ambil nilai detik
+              var ss = time.getSeconds().toString();
+              //tampilkan jam:menit:detik dengan menambahkan angka 0 jika angkanya cuma satu digit (0-9)
+              document.getElementById("clock").innerHTML = (sh.length==1?"0"+sh:sh) + ":" + (sm.length==1?"0"+sm:sm) + ":" + (ss.length==1?"0"+ss:ss);
+          }
+      </script>
       <?php
       date_default_timezone_set("Asia/Jakarta");
         function hari(){
@@ -70,6 +95,7 @@
         $tanggal = hari().", ". date("j M Y");
         echo $tanggal;
       ?>
+      <span id="clock"><?php print date('H:i:s'); ?></span>
     </div>
     <div class="top-bar-menu" style="color:yellow; padding-left:20px; font-size:14px;">
       <?php
