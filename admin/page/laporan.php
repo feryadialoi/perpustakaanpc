9c4b15546630a9f14086d5a104706d8da57aa630
Feryadi <feryadialoi@gmail.com>
2018-06-27 15:50:48 +0700

    uodate tampilan laporan and pengaturan : user and admin

--- admin/page/laporan.php
+++ admin/page/laporan.php
@@ -1,1 +1,5 @@
 ini page laporan
+
+  <div class="" style="text-align: center;padding-top: 100px;">
+    <p class="dashboard-text">COMING SOON!</p>
+  </div>
--- user/page/pengaturan.php
+++ user/page/pengaturan.php
@@ -1,1 +1,5 @@
 ini page pengaturan
+
+  <div class="" style="text-align: center;padding-top: 100px;">
+    <p class="dashboard-text">COMING SOON!</p>
+  </div>
