f09a7e08c1c18d808cf7991244b9a561238c4258
kakeru777 <mika.august9@gmail.com>
2018-07-11 09:42:30 +0700

    7

--- admin/page/buku/hapus.php
+++ admin/page/buku/hapus.php
@@ -1,15 +1,13 @@
+<!-- ini page hapus buku
+<div class="float-tombol">
+  <a class="btn btn-primary" href="?page=buku">Kembali</a>
+</div> -->
 <<?php
-<<<<<<< HEAD
   $id = $_GET['id'];
-  $conn->query("delete from tb_buku where id ='$id'");
+  $koneksi->query("delete from tb_buku where id ='$id'");
 ?>
-=======
-$id = $_GET['id'];
-$koneksi->query("delete from tb_buku where id ='$id'");
- ?>
->>>>>>> 1799c2e96067aa869ab8c1989a1df71ec006fe0d
 
- <script type="text/javascript">
- window.location.href="?page=buku";
- </script>
- 
+<script type="text/javascript">
+  alert("Data Berhasil Dihapus!")
+  window.location.href="?page=buku";
+</script>
